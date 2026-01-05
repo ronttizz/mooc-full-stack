3f9bdc84859b8d61e5887fcbf31825a77c345411
ronttizz <toni.nissinen@gmail.com>
2026-01-05 08:52:49 +0200

    Remove commented out original data

--- part1/courseInformation/src/App.jsx
+++ part1/courseInformation/src/App.jsx
@@ -4,12 +4,6 @@ import Total from './Total'
 
 const App = () => {
   const course = 'Half Stack application development'
-  // const part1 = 'Fundamentals of React'
-  // const exercises1 = 10
-  // const part2 = 'Using props to pass data'
-  // const exercises2 = 7
-  // const part3 = 'State of a component'
-  // const exercises3 = 14
   const parts = [
     {name: 'Fundamentals of React', exercises: 10},
     {name: 'Using props to pass data', exercises: 7},
