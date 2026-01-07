3e9ce5a39561f3681dad13738b7ba9d6f569c905
ronttizz <toni.nissinen@gmail.com>
2026-01-07 11:35:20 +0200

    Add console log msgs

--- part1/courseMaterialTesting/src/App.jsx
+++ part1/courseMaterialTesting/src/App.jsx
@@ -4,10 +4,20 @@ import Button from './Button'
 
 const App = () => {
   const [ counter, setCounter ] = useState(0)
+  console.log('rendering with counter value', counter)
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const setToZero = () => setCounter(0)
-  const decreaseByOne = () => setCounter(counter -1)
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+  const decreaseByOne = () => {
+    console.log('decreaseing, value before', counter)
+    setCounter(counter -1)
+  }
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
 
   return (
     <div>
