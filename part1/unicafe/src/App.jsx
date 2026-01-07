096738e405c66735bfe02625c564e7a975f440ac
ronttizz <toni.nissinen@gmail.com>
2026-01-07 14:28:26 +0200

    Add button component and render total and average points

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,20 +1,33 @@
 import { useState } from 'react'
+import Button from './Button'
 
-function App() {
+const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  const average = () => {
+    if (good + neutral + bad === 0) {
+      return 0
+    } else {
+      return (good - bad) / (good + neutral + bad)
+    }
+  }
 
   return (
   <div>
     <h1>give feedback</h1>
     <div>
-      buttons
+      <Button onClick={() => setGood(good + 1)} text="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="bad" />
     </div>
     <h1>statistics</h1>
     <p>good {good}</p>
     <p>neutral {neutral}</p>
     <p>bad {bad}</p>
+    <p>all {good + neutral + bad}</p>
+    <p>average {average()}</p>
   </div>
   )
 }
