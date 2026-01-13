fabd97f70ecc8adbfe07f087e9cc91a4ebf220d8
ronttizz <toni.nissinen@gmail.com>
2026-01-13 12:37:34 +0200

    exercise 2.6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,19 +6,33 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const personsCopy = [...persons]
+    setPersons(personsCopy.concat({name: newName}))
+    setNewName('')
+  }
+
+  const handleChange = (event) => {
+    setNewName(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: <input onChange={handleChange} value={newName} />
         </div>
+        <div>debug: {newName}</div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person => 
+        <div key={person.name}>{person.name}</div>
+      )}
     </div>
   )
 }
