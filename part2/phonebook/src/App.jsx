66ad6b391b5aee8b02b0f2e33ae6e41adbc08ae8
ronttizz <toni.nissinen@gmail.com>
2026-01-13 13:05:14 +0200

    exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,48 +1,57 @@
 import { useState } from 'react'
 
 const App = () => {
+  const emptyPerson = {
+    name: '',
+    number: ''
+  }
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    {
+      name: 'Arto Hellas',
+      number: '040-1234567'
+    }
   ]) 
-  const [newName, setNewName] = useState('')
+  const [newPerson, setNewPerson] = useState(emptyPerson)
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    let found = false
-    persons.forEach(person => {
-      if (newName === person.name) {
-        found = true
-      }
-    })
-    if (found) {
-      alert(`${newName} is already added to phonebook`)
-      setNewName('')
+    if (persons.some((person) => newPerson.name === person.name)) {
+      alert(`${newPerson.name} is already added to phonebook`)
+      setNewPerson(emptyPerson)
     } else {
       const personsCopy = [...persons]
-      setPersons(personsCopy.concat({name: newName}))
-      setNewName('')
+      setPersons(personsCopy.concat({
+        name: newPerson.name,
+        number: newPerson.number
+      }))
+      setNewPerson(emptyPerson)
     }
   }
 
-  const handleChange = (event) => {
-    setNewName(event.target.value)
+  const handleNameChange = (event) => {
+    const newPersonCopy = {...newPerson}
+    newPersonCopy.name = event.target.value
+    setNewPerson(newPersonCopy)
+  }
+
+  const handleNumberChange = (event) => {
+    const newPersonCopy = {...newPerson}
+    newPersonCopy.number = event.target.value
+    setNewPerson(newPersonCopy)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
-        <div>
-          name: <input onChange={handleChange} value={newName} />
-        </div>
-        <div>debug: {newName}</div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+        <div>name: <input onChange={handleNameChange} value={newPerson.name} /></div>
+        <div>number: <input onChange={handleNumberChange} value={newPerson.number} /></div>
+        <div><button type="submit">add</button></div>
+        <div>debug: {newPerson.name} {newPerson.number}</div>
       </form>
       <h2>Numbers</h2>
       {persons.map(person => 
-        <div key={person.name}>{person.name}</div>
+        <div key={person.name}>{person.name} {person.number}</div>
       )}
     </div>
   )
