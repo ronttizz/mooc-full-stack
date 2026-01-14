76fd55633d9e078c7ca2f7012d0da9909ce14d59
ronttizz <toni.nissinen@gmail.com>
2026-01-14 12:10:34 +0200

    fetch data from mock db instead of hardcoded to component

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Person from './components/Person'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
@@ -10,12 +11,7 @@ const App = () => {
     number: '',
     id: null
   }
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newPerson, setNewPerson] = useState(emptyPerson)
   const [searchTerm, setSearchTerm] = useState('')
 
@@ -51,6 +47,17 @@ const App = () => {
   const handleSearchTermChange = (event) => {
     setSearchTerm(event.target.value)
   }
+
+  // Data fetching
+  const fetchData = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(promise => {
+        setPersons(promise.data)
+      })
+  }
+
+  useEffect(fetchData, [])
 
   return (
     <div>
