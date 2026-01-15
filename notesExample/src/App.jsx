2052a4b06b3e0904472014bae5794d1a15c98b9f
ronttizz <toni.nissinen@gmail.com>
2026-01-15 16:11:03 +0200

    testing errors and catching them

--- notesExample/src/App.jsx
+++ notesExample/src/App.jsx
@@ -43,6 +43,12 @@ const App = () => {
       .then(returnedNote => {
         setNotes(notes.map(note => note.id === id ? returnedNote : note))
       })
+      .catch(error => {
+        alert(
+          `the note '${note.content}' was already deleted from server`
+        )
+        setNotes(notes.filter(n => n.id !== id))
+      })
   }
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important)
