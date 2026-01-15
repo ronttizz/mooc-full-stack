d489e0caa9611e64847e7de9959441819c0bedf0
ronttizz <toni.nissinen@gmail.com>
2026-01-15 10:07:13 +0200

    mock post example on json-server

--- notesExample/src/App.jsx
+++ notesExample/src/App.jsx
@@ -20,12 +20,15 @@ const App = () => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
-      important: Math.random() > 0.5,
-      id: String(notes.length + 1),
+      important: Math.random() > 0.5
     }
 
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+  axios
+    .post('http://localhost:3001/notes', noteObject)
+    .then(response => {
+      setNotes(notes.concat(response.data))
+      setNewNote('')
+    })
   }
 
   const handleNoteChange = (event) => {
