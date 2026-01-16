70005d6bdd43919a62c03fbebfcfb3538a17ae15
ronttizz <toni.nissinen@gmail.com>
2026-01-16 18:24:36 +0200

    install axios and make initial call for testing when rendering the app and setup state for the response

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,7 +1,22 @@
+import { useEffect } from "react"
+import axios from 'axios'
+import { useState } from "react"
+
 const App = () => {
+  const [countries, setCountries] = useState([])
+
+  const fetchData = () => {
+    axios
+      .get('https://studies.cs.helsinki.fi/restcountries/api/all')
+      .then(res => {
+        console.log(res.data)
+      })
+  }
+  useEffect(fetchData,[])
+
   return (
     <div>
-      find countries <input></input>
+      <div>find countries <input></input></div>
     </div>
   )
 }
