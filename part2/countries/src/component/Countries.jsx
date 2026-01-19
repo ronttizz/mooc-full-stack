717764bf259c7c7f1c8af716021eff9403bc74fe
ronttizz <toni.nissinen@gmail.com>
2026-01-19 10:44:28 +0200

    pass onClick functionality to component to manipulate searchTerm

--- part2/countries/src/component/Countries.jsx
+++ part2/countries/src/component/Countries.jsx
@@ -1,6 +1,6 @@
 import Country from './Country'
 
-const Countries = ({countries}) => {
+const Countries = ({countries, onClickShow}) => {
     console.log(countries)
     if (countries.length > 10) {
         return <p>Too many matches, specify another filter</p>
@@ -8,7 +8,7 @@ const Countries = ({countries}) => {
     return (
         <div>
             {countries.length > 1 
-                ? countries.map(country => <p key={country.cca3}>{country.name.common}</p>)
+                ? countries.map(country => <p key={country.cca3}>{country.name.common} <button onClick={() => onClickShow(country.name.common)} >Show</button></p>)
                 : countries.map(country => <Country country={country} key={country.cca3} />)
             }
         </div>
