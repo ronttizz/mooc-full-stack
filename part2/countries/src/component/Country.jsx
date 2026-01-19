b5a980253605fa0af8b7c7dd56646698c9a20fbe
ronttizz <toni.nissinen@gmail.com>
2026-01-19 10:43:49 +0200

    display information of the country

--- part2/countries/src/component/Country.jsx
+++ part2/countries/src/component/Country.jsx
@@ -1,7 +1,16 @@
 const Country = ({country}) => {
     return (
         <div>
-            <h1>{country.name.official}</h1>
+            <h1>{country.name.common}</h1>
+            <p>
+                Capital: {country.capital[0]} <br />
+                Area: {country.area}
+            </p>
+            <h2>Languages</h2>
+            <ul>
+                {Object.values(country.languages).map(lang => <li key={lang}>{lang}</li>)}
+            </ul>
+            <img src={country.flags.svg} alt={country.name.official} />
         </div>
     )
 }
