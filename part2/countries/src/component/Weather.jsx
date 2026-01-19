a8cc94beff4c2a3eaeba229e94c6beaf20d6a28f
ronttizz <toni.nissinen@gmail.com>
2026-01-19 11:51:02 +0200

    remove as not provided

--- part2/countries/src/component/Weather.jsx
+++ part2/countries/src/component/Weather.jsx
@@ -1,7 +1,7 @@
 const Weather = ({lat, lng}) => {
     return (
         <div>
-          <h1>Weather in {capital}</h1>
+          <h1>Weather in </h1>
           <p>Temperature 0 Celsius</p>
           <p>PIC HERE</p>
           <p>Wind 0 m/s</p>
