9d53e69b78ef702812ab95114613d7c15e65a946
ronttizz <toni.nissinen@gmail.com>
2026-01-20 09:32:41 +0200

    remove console log

--- part2/countries/src/component/Countries.jsx
+++ part2/countries/src/component/Countries.jsx
@@ -1,7 +1,6 @@
 import Country from './Country'
 
 const Countries = ({countries, onClickShow}) => {
-    console.log(countries)
     if (countries.length > 10) {
         return <p>Too many matches, specify another filter</p>
     }
