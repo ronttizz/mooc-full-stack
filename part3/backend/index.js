3652f2b50ce0d1eee0d3ca5de5e35bfe959f0222
ronttizz <toni.nissinen@gmail.com>
2026-01-21 21:18:44 +0200

    remove console log, add calculation for next ID and saving the note

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -47,8 +47,15 @@ app.delete('/api/notes/:id', (request, response) => {
 })
 
 app.post('/api/notes', (request, response) => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => Number(n.id))) 
+    : 0
+
   const note = request.body
-  console.log(note)
+  note.id = String(maxId + 1)
+
+  notes = notes.concat(note)
+
   response.json(note)
 })
 
