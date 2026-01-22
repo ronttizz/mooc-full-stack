264314c637cc4f1a9788da65a1a6d815d7f24b63
ronttizz <toni.nissinen@gmail.com>
2026-01-22 10:01:47 +0200

    creae the base file for responses

---
+++ part3/phonebook-backend/index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const PORT = 3001
+
+const persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.use(express.json())
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
+app.get('/', (request, response) => {
+  console.log(`GET request handled`)
+  response.send('<h1>Hello</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.send(persons)
+})
