e8468fa071ed4e296a49b8a2dd1445a90b7ca842
ronttizz <toni.nissinen@gmail.com>
2026-01-27 10:18:42 +0200

    refactor port

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 const currentDate = new Date()
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) || '- no content' })
