a710eb1afdba83ff4408058dce6a34e43dfd5564
ronttizz <toni.nissinen@gmail.com>
2026-01-30 10:39:58 +0200

    create CLI tool for phonebook db. Exercise 3.12

---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Missing arguments. Give at least password to get contacts.')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const url = `mongodb+srv://toninissinen_db_user:${password}@cluster0.xunuwbe.mongodb.net/phonebookApp?appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url, { family: 4 })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 3) {
+  Person.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+} else if (process.argv.length === 4) {
+  console.log('Name or number missing')
+} else if (process.argv.length === 5) {
+  const name = process.argv[3]
+  const number = process.argv[4]
+
+  const person = new Person({
+    name: name,
+    number: number,
+  })
+
+  person.save().then(result => {
+    console.log(`added ${name} ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+} else {
+  console.log('Too many arguments. If name contains whitespace remember to enclosed it in quotes.')
+  process.exit(1)
+}
+
