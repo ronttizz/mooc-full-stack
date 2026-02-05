6c7acea8bc0a85445e4458521f8710c3dc1e1af6
ronttizz <toni.nissinen@gmail.com>
2026-02-05 09:42:58 +0200

    add some rules

--- part3/backend/eslint.config.mjs
+++ part3/backend/eslint.config.mjs
@@ -16,9 +16,17 @@ export default [
     },
     rules: { 
       '@stylistic/js/indent': ['error', 2],
-      '@stylistic/js/linebreak-style': ['error', 'unix'],
+      '@stylistic/js/linebreak-style': ['error', 'windows'],
       '@stylistic/js/quotes': ['error', 'single'],
       '@stylistic/js/semi': ['error', 'never'],
+      eqeqeq: 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'no-console': 'off',
     }, 
+  },
+  { 
+    ignores: ['dist/**'], 
   },
 ]
