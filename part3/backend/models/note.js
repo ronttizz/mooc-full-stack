5ab95dce4a38560bb1294db32562239ae5dd9c4d
ronttizz <toni.nissinen@gmail.com>
2026-02-06 09:57:34 +0200

    Extract things to own places and use them

--- part3/backend/models/note.js
+++ part3/backend/models/note.js
@@ -3,15 +3,6 @@ mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URI
 
-console.log('connecting to', url)
-mongoose.connect(url, { family: 4 })
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
--- part3/backend/utils/config.js
+++ part3/backend/utils/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGODB_URI
+let PORT = process.env.PORT
+let MONGODB_URI = process.env.MONGODB_URI
 
 module.exports = { MONGODB_URI, PORT }
