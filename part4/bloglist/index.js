21b67f1412b6f9898fdc6537798be72071d83d62
ronttizz <toni.nissinen@gmail.com>
2026-02-06 13:02:34 +0200

    refactor and end some logging

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const app = express()
 
 const config = require('./utils/config')
 const Blog = require('./models/blog')
 
+const app = express()
+
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl, { family: 4 })
+  .then(_result => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('Error connecting to MongoDB:', error.message)
+  })
 
 app.use(express.json())
 
