e540612bd6db64e88c1325fd6af88e46ba92731f
ronttizz <toni.nissinen@gmail.com>
2026-02-09 12:41:32 +0200

    add tests and done until exercise 4.6*

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -8,3 +8,37 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
 })
+
+describe('total likes', () => {
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes([])
+    assert.strictEqual(result, 0)
+  })
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listHelper.listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+  
+  test('of a bigger lis is calculated right', () => {
+    const result = listHelper.totalLikes(listHelper.blogs)
+    assert.strictEqual(result, 36)
+  })
+})
+
+describe('favorite blog', () => {
+  test('of empty list is zero', () => {
+    const result = listHelper.favoriteBlog([])
+    assert.strictEqual(result, 0)
+  })
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.favoriteBlog(listHelper.listWithOneBlog)
+    assert.deepStrictEqual(result, listHelper.listWithOneBlog[0])
+  })
+  
+  test('of a bigger lis is calculated right', () => {
+    const result = listHelper.favoriteBlog(listHelper.blogs)
+    assert.strictEqual(result, listHelper.blogs[2])
+  })
+})
