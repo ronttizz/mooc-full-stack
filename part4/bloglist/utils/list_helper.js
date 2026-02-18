030b5d4376f93b41729824d2e0a6758f5d946d29
ronttizz <toni.nissinen@gmail.com>
2026-02-18 09:33:14 +0200

    remove duplicate entry from the list of blogs

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -85,14 +85,6 @@ const blogs = [
     __v: 0
   },
   {
-    _id: "5a422aa71b54a676234d17f8",
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-    __v: 0
-  },
-  {
     _id: "5a422b3a1b54a676234d17f9",
     title: "Canonical string reduction",
     author: "Edsger W. Dijkstra",
