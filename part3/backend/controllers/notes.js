c6527b4b3e548ebb454dc123cebf9da68c9b7e42
ronttizz <toni.nissinen@gmail.com>
2026-02-20 11:24:37 +0200

    handle user ids in router and refactor REST client requests to account these changes

--- part3/backend/controllers/notes.js
+++ part3/backend/controllers/notes.js
@@ -1,5 +1,6 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 notesRouter.get('/', async (request, response) => {
   const notes = await Note.find({})
@@ -29,12 +30,23 @@ notesRouter.delete('/:id', (request, response, next) => {
 notesRouter.post('/', async (request, response, next) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+  
+  if (!user) {
+    return response.status(400).json({ error: 'userId missing or not valid' })
+  }
+
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user._id
   })
 
   const savedNote = await note.save()
+  
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+  
   response.status(201).json(savedNote)
 })
 
