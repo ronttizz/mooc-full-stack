62d5a7453a17906ff8b9c187477790fc77ff9af7
ronttizz <toni.nissinen@gmail.com>
2026-02-24 10:17:12 +0200

    use middleware

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const middleware = require('./utils/middleware')
 
 const app = express()
 
@@ -18,5 +19,8 @@ mongoose.connect(config.MONGODB_URI, { family: 4 })
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
